947c9edcf9262e815d2f19201019d2cd270aa584
MitNy <leemj314@gmail.com>
2020-01-21 02:24:57 +0000

    Add solution

---
+++ ComparetheTriplets.php
@@ -0,0 +1,35 @@
+<?php
+
+// Complete the compareTriplets function below.
+function compareTriplets($a, $b) {
+    $alice = 0;
+    $bob = 0;
+    $result = [];
+    for( $i=0; $i<count($a); $i++ ) {
+        if( $a[$i] < $b[$i] ) {
+            $bob++;
+        }
+        else if( $a[$i] > $b[$i] ) {
+            $alice++;
+        }
+    }
+    $result = [$alice, $bob];
+    return $result;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$a_temp = rtrim(fgets(STDIN));
+
+$a = array_map('intval', preg_split('/ /', $a_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$b_temp = rtrim(fgets(STDIN));
+
+$b = array_map('intval', preg_split('/ /', $b_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = compareTriplets($a, $b);
+
+fwrite($fptr, implode(" ", $result) . "\n");
+
+fclose($fptr);
+
